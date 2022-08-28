4af7ac13c51b6ce63129d3d13de40bda11c432e9
OK <@mail.ru>
2022-08-29 00:05:17 +0300

    add 001

---
+++ 001/Program.cs
@@ -0,0 +1,45 @@
+﻿/*/Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9/*/
+
+
+class Program
+{
+    static void FillArray(double[,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                arr[i, j] = Convert.ToDouble(new Random().NextDouble() * 10 - 5);
+            }
+        }
+    }
+    static void PrintArray(double[,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                arr[i, j] = Math.Round(arr[i, j], 1);
+                Console.Write(arr[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк m ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите количество столбцов n ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"m={m},n={n}");
+        double[,] array = new double[m, n];
+        FillArray(array);
+        PrintArray(array);
+    }
+}
+
---
+++ 002/Program.cs
@@ -0,0 +1,52 @@
+﻿/*/Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет/*/
+
+class Program
+{
+    static void FillArray(int[,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                arr[i, j] = new Random().Next(0, 10);
+            }
+        }
+    }
+    static void PrintArray(int[,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите номер строки ");
+        int numberrow = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите номер столбца ");
+        int numbercolumn = Convert.ToInt32(Console.ReadLine());
+        int[,] array = new int[3, 4];
+        FillArray(array);
+        PrintArray(array);
+        if (numberrow > array.GetLength(0) || numbercolumn > array.GetLength(1))
+        {
+            Console.WriteLine("Такого элемента нет");
+        }
+        else
+        {
+            Console.WriteLine($"Значение элемента {array[numberrow - 1, numbercolumn - 1]}");
+        }
+    }
+}
+
